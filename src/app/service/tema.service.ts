f3edd4b026a6c118336ab29ddbf7a457165cacd7
Ana Paula Souza <anapaula_sdias@hotmail.com>
2021-10-11 20:13:41 -0300

    21 PUT e DELETE de Tema

---
+++ src/app/service/tema.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TemaService {
+
+  constructor(
+    private http: HttpClient) { }
+
+    token ={
+      headers: new HttpHeaders().set('Authorization', environment.token)
+    }
+
+    getAllTema(): Observable<Tema[]>{
+      return this.http.get<Tema[]>('https://blogpessoalanasouzadias.herokuapp.com/tema', this.token)
+    }
+
+    PostTema(tema:Tema): Observable<Tema>{
+      return this.http.post<Tema>('https://blogpessoalanasouzadias.herokuapp.com/tema', tema,this.token)
+
+    }
+
+    putTema(tema:Tema): Observable<Tema>{
+      return this.http.put<Tema>('https://blogpessoalanasouzadias.herokuapp.com/tema', tema,this.token)
+    }
+
+    deleteTema(id:number){
+    return this.http.delete<Tema>(`https://blogpessoalanasouzadias.herokuapp.com/tema/${id}`, this.token)
+    }
+
+}
--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-tema',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemaComponent implements OnInit {
 
-  constructor() { }
+  tema:Tema = new Tema()
+  listaTemas: Tema[]
 
-  ngOnInit(): void {
+  constructor(
+    //com letra minuscula router e temaService pois são variaveis
+    private router: Router,
+    private temaService: TemaService
+
+  ) { }
+
+  ngOnInit() {
+    if (environment.token == '') {
+      this.router.navigate(['/entrar'])
+    }
+
+    this.findAllTemas()
+  }
+
+  findAllTemas(){
+      this.temaService.getAllTema().subscribe((resp: Tema[])=> {
+        this.listaTemas = resp
+      })
+  }
+
+  cadastrar(){
+        this.temaService.PostTema(this.tema).subscribe((resp: Tema)=>{
+              this.tema = resp
+              alert('Tema cadastrado com sucesso!')
+              this.findAllTemas()
+              this.tema = new Tema()
+        })
   }
 
 }
